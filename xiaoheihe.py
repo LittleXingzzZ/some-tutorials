d30ae11154b73100ad0665940ded38f407157889
LittleXingzzZ <55964606+LittleXingzzZ@users.noreply.github.com>
2023-04-12 23:34:07 +0800

    Update xiaoheihe.py

--- xiaoheihe.py
+++ xiaoheihe.py
@@ -1,12 +1,13 @@
 import requests
 import time
+import os
 
 url = "https://api.xiaoheihe.cn/task/sign_v3/get_sign_state"
 
 headers = {
     "User-Agent": "xiaoheihe/1.3.240 (iPhone; iOS 16.3.1; Scale/3.00)",
     "Referer": "http://api.maxjia.com/",
-    "Cookie": "pkey=MTY4MTA0MjI3NC42NV8zODkwOTk5dWt0bXZsdXNkY2h1d3ZjYw____;hkey=c71160c0b254725a389bce0fa5170caf;x_xhh_tokenid=BTJynmFUFIOsV/VCbNb7mgfqVK9a/jmv3oloVmYr00UTYqfxkOG1gXdsVG/iAF8htqixuUCHb4X28uJ/fiybY/A==",
+    "Cookie": os.environ.get("Box_Cookie"),
 }
 
 params = {
@@ -15,14 +16,14 @@ params = {
     "os_version": "16.3.1",
     "_time": str(int(time.time())),
     "version": "1.3.240",
-    "device_id": "1EC67981-9B5F-4CF4-B9A2-EDE8AC02D11A",
-    "heybox_id": "3890999",
-    "nonce": "4ftIXtOhw99JcQjlw6NYX2dicggGKF01",
+    "device_id": os.environ.get("Box_device_id"),
+    "heybox_id": os.environ.get("Box_heybox_id"),
+    "nonce": os.environ.get("Box_nonce"),
     "dw": "428",
     "x_app": "heybox",
     "x_client_type": "mobile",
     "x_os_type": "iOS",
-    "hkey": "FJ5K694",
+    "hkey": os.environ.get("Box_hkey"),
 }
 
 response = requests.get(url, headers=headers, params=params)
